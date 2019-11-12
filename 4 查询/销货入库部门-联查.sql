c47315545f827c1a5db7ca2336b8a29031d5065d
Harvey <harveyzyh@hotmail.com>
2019-11-12 10:29:36 +0800

    20191112

--- 4 查询/销货入库部门-联查.sql
+++ 4 查询/销货入库部门-联查.sql
@@ -3,8 +3,8 @@ declare @StartDate varchar(20)
 declare @EndDate varchar(20)
 
 /*销货单的单据起始与结束日期*/
-set @StartDate = '20190901'
-set @EndDate = '20190931'
+set @StartDate = '20191001'
+set @EndDate = '20191031'
 
 SELECT DISTINCT A1 AS 销货单号, A2 AS 客户简称, 
 A4 AS 品号, A5 AS 品名, A6 AS 规格, B2 AS 仓库名称, 
