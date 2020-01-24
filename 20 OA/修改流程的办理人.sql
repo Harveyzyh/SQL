8daf346ae18337bc20ad3d893be8d255fb17ad52
Harvey <harveyzyh@hotmail.com>
2020-01-24 08:40:13 +0800

    20200124

--- 20 OA/修改流程的办理人.sql
+++ 20 OA/修改流程的办理人.sql
@@ -1,5 +1,5 @@
 Select * from flow_type -- 流程类别小类
--- where flow_id = 3136
+where flow_id = 3010
 order by FLOW_ID
 
 
@@ -9,7 +9,7 @@ select * from flow_process -- 流程步骤设计表
 -- update flow_process set prcs_dept = CONCAT(prcs_dept,'65,')
 where 1=1
 -- and FLOW_ID = 3140 -- 流程编号，可以再flow_type表里获得
-and PRCS_ID = 1  -- 步骤序号
+and PRCS_ID = 2  -- 步骤序号
 and prcs_dept like '%26%' and prcs_dept not like '%65%'
 
 
